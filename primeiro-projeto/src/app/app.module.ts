3bc583a037f40834fb8f5c852b7e369ea5e9f9d1
Lucas <lucsousan@gmail.com>
2020-10-27 04:42:52 -0300

    novo

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent
   ],
   imports: [
     BrowserModule,
